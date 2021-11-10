2ceed50f56dd1fd64d4b5a2965b0cc3c82167a47
exark <ahmedmohamedali.benhamouda@esprit.tn>
2021-11-10 19:14:12 +0100

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { RestComponent } from './rest/rest.component';
+import { HttpClientModule } from "@angular/common/http";
+import {CommonModule} from '@angular/common';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RestComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
